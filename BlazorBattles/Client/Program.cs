d328be5f19b26d7c3186782523fecb36ae2280a5
ZeoxZeos <zenoniwanicki@hotmail.com>
2021-12-01 22:09:35 +0000

    Add project files.

---
+++ BlazorBattles/Client/Program.cs
@@ -0,0 +1,39 @@
+using BlazorBattles.Client.Services;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Blazored.Toast;
+using Microsoft.AspNetCore.Components.Authorization;
+using Blazored.LocalStorage;
+
+namespace BlazorBattles.Client
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+            builder.RootComponents.Add<App>("#app");
+
+            builder.Services.AddBlazoredToast();
+            builder.Services.AddBlazoredLocalStorage();
+            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            builder.Services.AddScoped<IBananaService, BananaService>();
+            builder.Services.AddScoped<IUnitService, UnitService>();
+            builder.Services.AddScoped<AuthenticationStateProvider, CustomAuthStateProvider>();
+            builder.Services.AddOptions();
+            builder.Services.AddAuthorizationCore();
+            builder.Services.AddScoped<IAuthService, AuthService>();
+            builder.Services.AddScoped<ILeaderboardService, LeaderboardService>();
+            builder.Services.AddScoped<IBattleService, BattleService>();
+
+            await builder.Build().RunAsync();
+        }
+    }
+}
